92d538513316eefc0cdbe8ff0114835fe70976bd
johnmcnally7 <johnmcnally7@John-McNallys-MacBook-Pro.local>
2017-02-06 10:49:41 -0500

    Small fixes

---
+++ js/script.js
@@ -0,0 +1,10 @@
+  //falcons button
+$('.falcons').on('click', function() {
+  $('.responseimage1').attr('src', 'http://binaryapi.ap.org/8ac7986e05c9475b85b9a700556a4399/460x.jpg');
+  $('.message').text('Great Choice!');
+});
+
+//evil patriots button
+$('.patriots').on('click', function() {
+  $('.responseimage1').attr('src', 'https://media.giphy.com/media/3o7qDWoeN6PaJ6Z6RW/giphy.gif');
+  $('.message').text('It is not too late to change yourmind');
