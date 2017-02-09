2ce01f828b515c0290ca4a76f01788c57ac6947c
johnmcnally7 <johnmcnally7@JohnMcNallysMBP.home>
2017-02-09 12:33:52 -0500

    button name change

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-  //falcons button
-$('.yes').on('click', function() {
+  //yes button
+$('.yesbutton').on('click', function() {
   $('.responseimage1').attr('src', 'http://binaryapi.ap.org/8ac7986e05c9475b85b9a700556a4399/460x.jpg');
   $('.message').text('Great Choice!');
 });
 
 //Thank god you can sympathize with a falcons fan
-$('.no').on('click', function() {
+$('.nobutton').on('click', function() {
   $('.responseimage1').attr('src', 'https://media.giphy.com/media/3o7qDWoeN6PaJ6Z6RW/giphy.gif');
   $('.message').text('I guess the Falcons will be back next year...');
 });
