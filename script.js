82ccba340e590721f51fbe04ae7d08cb535ac1e1
johnmcnally7 <johnmcnally7@JohnMcNallysMBP.home>
2017-02-09 12:17:50 -0500

    Post Superbowl
    script.js update

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
   //falcons button
-$('.falcons').on('click', function() {
+$('.yes').on('click', function() {
   $('.responseimage1').attr('src', 'http://binaryapi.ap.org/8ac7986e05c9475b85b9a700556a4399/460x.jpg');
   $('.message').text('Great Choice!');
 });
 
 //evil patriots button
-$('.patriots').on('click', function() {
+$('.no').on('click', function() {
   $('.responseimage1').attr('src', 'https://media.giphy.com/media/3o7qDWoeN6PaJ6Z6RW/giphy.gif');
   $('.message').text('It is not too late to change yourmind');
 });
